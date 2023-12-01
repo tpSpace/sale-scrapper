11966fd36a640d0c2da9cdd41b98c3f224cecbab
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-02 00:29:35 +0700

    Make a simple structure of the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import SideBar from './components/sidebar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="navbar">
+          <SideBar />
+        </div>
+
+        <div className="content">
+          <div className="searchbar">
+            <h1>search here?</h1>
+          </div>
+          <div className="item">
+            <h1>Hii</h1>
+          </div>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            <a href="">Home</a>
+            <a href="">Login</a>
+            <a href="">Favorite</a>
+        </div>
+    );
+};
+
+export default SideBar;
