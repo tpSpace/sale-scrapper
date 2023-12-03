41f0e3d16260edfec63abb8fff0851c70c930d49
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-03 14:05:03 +0700

    Complete a simple routing

---
+++ src/components/structure/structure.jsx
@@ -0,0 +1,27 @@
+import SideBar from "../sidebar/sidebar"
+import PropTypes from 'prop-types'
+
+function Structure({children}) {
+  return (
+    <div className="container">
+            <div className="navbar">
+                <SideBar />
+            </div>
+
+            <div className="content">
+                <div className="searchbar">
+                    <h1>search here?</h1>
+                </div>
+
+                <div className="item">
+                    {children}
+                </div>
+            </div>
+
+        </div>
+  )
+}
+Structure.propTypes = {
+    children: PropTypes.node,
+};
+export default Structure
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import Structure from "../../components/structure/structure"
+
+const Home = () => {
+    return (
+        <div>
+            <Structure>
+                <h2>Home</h2>
+            </Structure>
+        </div>
+    )
+}
+
+
+
+export default Home
