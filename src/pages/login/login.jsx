f3fb60f525a8c28e5e2a321d5e9b9a2e6b86a3d0
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-03 16:43:54 +0700

    init login mechanism

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,63 @@
 import Structure from '../../components/structure/structure'
+import axios from 'axios'
+import './index.css';
+import { useState } from 'react';
 
 function Login() {
-  return (
-    <div>
-        <Structure>
-            <h2>Login</h2>
-        </Structure>
-    </div>
-  )
+
+    const [user,setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    const onInputChange = (e) => {
+        setUser({...user, [e.target.id]: e.target.value});
+    }
+
+    const handleSubmit =async (e) => {
+        e.preventDefault();
+        await axios.post("http://api.captechvn.com/login").then((res)=>{
+            console.log(res.data)
+            if (res.data.status === "success") {
+                localStorage.setItem("token", res.data.token);
+                localStorage.setItem("user", JSON.stringify(res.data.user));
+                window.location.href = "/";
+                window.location.reload();
+            }
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    return (
+        <div >
+            <Structure>
+                <div className="login">
+                    <div className="login-container">
+                        <form
+                            onSubmit={handleSubmit}
+                        >
+                            <h1>Login</h1>
+                            <br />
+                            <input 
+                                type="email" 
+                                placeholder="Username" 
+                                onChange={(e) => onInputChange(e)}
+                                required
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Password" 
+                                onChange={(e) => onInputChange(e)}
+                                required
+                            />
+                            <button type="submit">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </Structure>
+        </div>
+    )
 }
 
 export default Login
